d9b34d55a35dc3072eda4859581be21c244e0021
user.mail <arjunlubana562@gmail.com>
2020-06-30 06:23:15 +0300

    Added Side NavBar toggle

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+/* Toggle Navigation Menu */
+
+function toggleMenu(){
+    document.getElementById("nav").classList.add("active");
+}
+function toggleMenuOff(){
+    document.getElementById("nav").classList.remove("active");
+}
