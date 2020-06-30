0ebda6d4814b626cad04d3fe516cfcc1626c9904
user.mail <arjunlubana562@gmail.com>
2020-06-30 16:54:34 +0300

    Styled themain section

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,15 @@ function toggleMenu(){
 function toggleMenuOff(){
     document.getElementById("nav").classList.remove("active");
 }
+
+/* Toggle Images */
+
+let prevID = "image1";
+document.getElementById(prevID).style.opacity = 0.7;
+function imageToggle(ID){
+    document.getElementById(prevID).style.opacity = 1;
+    document.getElementById(ID).style.opacity = 0.7;
+    let url = "images/"+ID+".png"
+    document.getElementById("main-image").src = url;
+    prevID = ID;
+}
