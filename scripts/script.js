3db0185d6bfbf0f4ca3bdb4b08c9baed7a7fa7fe
user.mail <arjunlubana562@gmail.com>
2020-07-01 10:23:30 +0300

    Added Media Query for 700px viewport

--- scripts/script.js
+++ scripts/script.js
@@ -10,10 +10,10 @@ function toggleMenuOff() {
 /* Toggle Images */
 
 let prevID = "image1";
-document.getElementById(prevID).style.opacity = 0.7;
+document.getElementById(prevID).style.background = "rgba(0,0,0,0.2)";
 function imageToggle(ID) {
-    document.getElementById(prevID).style.opacity = 1;
-    document.getElementById(ID).style.opacity = 0.7;
+    document.getElementById(prevID).style.background = "";
+    document.getElementById(ID).style.background = "rgba(0,0,0,0.2)";
     let url = "images/" + ID + ".png"
     document.getElementById("main-image").src = url;
     prevID = ID;
